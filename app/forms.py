2c3d8b6a0c9b3a33d4145ad858959fb9a96206c5
antoniouaa <antoniouaa@hotmail.com>
2020-09-25 02:20:15 +0100

    strugglebus

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class QueryForm(FlaskForm):
     location = StringField("Location", validators=[DataRequired()])
-    submit = SubmitField("Search")
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
 from app.forms import QueryForm
+import requests
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/index", methods=["GET", "POST"])
@@ -14,4 +15,11 @@ def index():
 
 @app.route("/<location>", methods=["GET"])
 def temp_fetch(location):
-    return render_template("weather_report.html", title="Report", location=location)
+    url = app.config["BASE_URL"]
+    key = app.config["API_KEY"]
+    params = {
+        "key" : key,
+        "q" : location,
+    }
+    weather_request = requests.get(f"{url}/current.json", params=params).json()
+    return render_template("weather_report.html", title="Report", location=location, response=weather_request)
